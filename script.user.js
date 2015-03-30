4a30c11178fa1edf5f344400e1dbff4dea1dc9f4
Mateusz Czupryniak <Kodekgames@gmail.com>
2015-03-30 14:15:10 +0200

    Improved minor things
    * Made title use the original title with informations
    * Added title to collected gifts page
    * Removed trailing spaces from code

--- script.user.js
+++ script.user.js
@@ -11,6 +11,8 @@
 // @grant		none
 // ==/UserScript==
 
+var orgTitle = "";
+
 var forumUrls = [];
 var checkedForumUrls = 0;
 
@@ -21,42 +23,43 @@ var validGiftUrls = [];
 var invalidGiftUrls = [];
 
 $(document).ready(function() {
-    
-    document.title = "Collecting gifts...";
-    
+
+    orgTitle = document.title;
+    document.title = orgTitle + " (collecting)";
+
     scanForTopics();
     asyncScanForGifts();
-    
+
 });
 
 function scanForTopics() {
     console.log("Scanning for topics...");
-    
+
     for (var i = 0; i < document.links.length; i++) {
         if (document.links[i].hostname === location.hostname) {
             var url = document.links[i].href;
-            
+
             if (url.indexOf('/discussion/') >= 0 && !containsString(forumUrls, url)) {
                 forumUrls.push(url);
             }
         }
     }
-    
+
     console.log("Scanned " + forumUrls.length + " topics...");
 }
 
 function asyncScanForGifts() {
     console.log("Scanning for gifts...");
-    
+
     for (var i = 0; i < forumUrls.length; i++) {
         $.ajax({
             url : forumUrls[i],
             success : function (source) {
                 var urls = findUrls(source);
-                
+
                 for (var i = 0; i < urls.length; i++) {
                     var url = urls[i];
-                    
+
                     if (url.indexOf('/giveaway/') >= 0 && !containsString(giftUrls, url)) {
                         giftUrls.push(url);
                     }
@@ -64,7 +67,7 @@ function asyncScanForGifts() {
             },
             complete: function() {
                 checkedForumUrls++;
-                
+
                 if (checkedForumUrls >= forumUrls.length) {
                     onGiftScanComplete();
                 }
@@ -75,13 +78,13 @@ function asyncScanForGifts() {
 
 function onGiftScanComplete() {
     console.log("Scanned " + giftUrls.length + " gifts...");
-    
+
     scanForValidGifts();
 }
 
 function scanForValidGifts() {
     console.log("Validating gifts...");
-    
+
     for (var i = 0; i < giftUrls.length; i++) {
         $.ajax({
             url : giftUrls[i],
@@ -94,7 +97,7 @@ function scanForValidGifts() {
             },
             complete: function() {
                 checkedGiftUrls++;
-                
+
                 if (checkedGiftUrls >= giftUrls.length) {
                     onValidGiftScanComplete();
                 }
@@ -105,48 +108,49 @@ function scanForValidGifts() {
 
 function onValidGiftScanComplete() {
     console.log("Validated " + validGiftUrls.length + " gifts...");
-    
-    document.title = "Finished collecting gifts...";
-    
+
+    document.title = orgTitle + " (done)";
+
     var linksWindow = window.open();
-    
+
+    linksWindow.document.write("<title>Collected Gifts - " + orgTitle + "</title>");
     linksWindow.document.write("<h1>Valid gifts:</h1><br>");
-    
+
     for (var i = 0; i < validGiftUrls.length; i++) {
         linksWindow.document.write("<a href='" + validGiftUrls[i] + "'>" + validGiftUrls[i] + "</a><br>");
     }
-    
+
     linksWindow.document.write("<h1>Invalid gifts:</h1><br>");
-    
+
     for (var i = 0; i < invalidGiftUrls.length; i++) {
         linksWindow.document.write("<a href='" + invalidGiftUrls[i] + "'>" + invalidGiftUrls[i] + "</a><br>");
     }
 }
 
 function isValidGift(source) {
-    
+
     // Already Entered check
     if (source.indexOf('sidebar__entry-insert is-hidden') >= 0) {
         return false;
     }
-    
+
     // No Entry check
     if (source.indexOf('entry_insert') == -1) {
         return false;
     }
-    
+
     return true;
 }
 
 function containsString(a, text) {
     var i = a.length;
-    
+
     while (i--) {
         if (a[i].toUpperCase() === text.toUpperCase()) {
             return true;
         }
     }
-    
+
     return false;
 }
 
@@ -154,14 +158,14 @@ function findUrls(text) {
     var source = (text || '').toString();
     var urlArray = [];
     var matchArray;
-    
+
     var regexToken = /(((ftp|https?):\/\/)[\-\w@:%_\+.~#?,&\/\/=]+)|((mailto:)?[_.\w-]+@([\w][\w\-]+\.)+[a-zA-Z]{2,3})/g;
-    
+
     while( (matchArray = regexToken.exec(source)) !== null ) {
         var token = matchArray[0];
-        
+
         urlArray.push(token);
     }
-    
+
     return urlArray;
-}    
+}
